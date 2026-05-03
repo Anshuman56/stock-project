2ec9fae28b5cdca18cb255ea9623607ebe2ef880
Anshuman56 <anshumandehury@yandex.com>
2026-05-03 11:01:29 +0530

    Add compare endpoint, 7 days data, updated clean_data.csv

--- main.py
+++ main.py
@@ -38,3 +38,27 @@ def get_summary(symbol: str):
         "52w_low": stock["52W_LOW"].min(),
         "avg_close": round(stock["CLOSE"].mean(), 2)
     }
+
+@app.get("/compare")
+def compare_stocks(symbol1: str, symbol2: str):
+    stock1 = df[df["SYMBOL"] == symbol1.upper()].fillna(0)
+    stock2 = df[df["SYMBOL"] == symbol2.upper()].fillna(0)
+
+    return {
+        "symbol1": {
+            "symbol": symbol1.upper(),
+            "avg_close": round(stock1["CLOSE"].mean(), 2),
+            "avg_daily_return": round(stock1["DAILY_RETURN"].mean() * 100, 2),
+            "volatility": round(stock1["VOLATILITY"].mean(), 4),
+            "52w_high": stock1["52W_HIGH"].max(),
+            "52w_low": stock1["52W_LOW"].min(),
+        },
+        "symbol2": {
+            "symbol": symbol2.upper(),
+            "avg_close": round(stock2["CLOSE"].mean(), 2),
+            "avg_daily_return": round(stock2["DAILY_RETURN"].mean() * 100, 2),
+            "volatility": round(stock2["VOLATILITY"].mean(), 4),
+            "52w_high": stock2["52W_HIGH"].max(),
+            "52w_low": stock2["52W_LOW"].min(),
+        }
+    }
